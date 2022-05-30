49f1a931aeb9c961e1e84034565bde550f95594e
vittorioaquilino <vittorioaquilino17@gmail.com>
2022-05-30 14:53:34 +0200

    add header-bottom

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,14 @@
 <template>
   <header class="ms_header">
       <div class="container">
+          <!-- header top -->
           <div class="ms_header-top">
+              <!-- language -->
               <div class="ms_header-language">
                   <span class="m-2">English</span>
                   <i class="fas fa-angle-down"></i>
               </div>
+              <!-- menu -->
               <div class="ms_header-menu">
                   <ul class="menu-list d-flex mt-2">
                       <li
@@ -14,6 +17,7 @@
                       >
                         <a href="">{{ element.text }}</a>
                       </li>
+                      <!-- social -->
                       <li><i class="fab fa-facebook"></i></li>
                       <li><i class="fab fa-twitter"></i></li>
                       <li><i class="fab fa-instagram"></i></li>
@@ -23,10 +27,43 @@
                   </ul>
               </div>
           </div>
+          <!-- /header top -->
       </div>
       <hr>
       <div class="container">
-          <div class="ms_header-bottom"></div>
+          <!-- header bottom -->
+          <div class="ms_header-bottom">
+              <!-- logo -->
+              <div class="ms_header-logo">
+                <img src="../assets/images/footer-logo2x-50x56.png" alt="">
+                <span class="fw-bold">MASTER</span> <span>STUDY</span>
+              </div>
+              <!-- search -->
+              <div class="ms_header-search">
+                  <!-- category -->
+                  <i class="fas fa-bars"></i>
+                  <span class="text-uppercase fw-bold">Category</span>
+                  <!-- input -->
+                  <div class="ms_input">
+                    <input type="text" placeholder="Search courses">
+                    <i class="fas fa-search"></i>
+                  </div>
+                  <!-- login -->
+                  <div class="ms_login">
+                      <i class="fas fa-bullhorn"></i>
+                      <span>Became and Instructor</span>
+                      <i class="fas fa-briefcase"></i>
+                      <span>For Entreprise</span>
+                      <div class="user-login">
+                          <i class="fas fa-user"></i>
+                          <span>Login</span>
+                          <button>sign up</button>
+                          <i class="far fa-bookmark"></i>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <!-- /header bottom -->
       </div>
   </header>
 </template>
@@ -67,6 +104,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../style/variables.scss";
 
 .ms_header {
     width: 100%;
@@ -74,7 +112,6 @@ export default {
 
     &-top {
         height: 70px;
-        border: 1px solid red;
         display: flex;
         justify-content: space-between;
         align-items: center;
@@ -88,7 +125,77 @@ export default {
 
     &-bottom {
         height: 130px;
-        border: 1px solid green;
+        display: flex;
+        align-items: center;
+        width: 100%;
+        margin: auto;
+
+        img {
+            background-color: $brand-primary-color;
+            margin-right: .5rem;
+        }
+
+        .ms_header-logo {
+
+            span {
+                font-size: 1.5rem;
+            }
+        }
+
+        .ms_header-search {
+            color: gray;
+            display: flex;
+            align-items: center;
+            margin-left: 2rem;
+
+            span {
+                font-size: .7rem;
+            }
+
+            input {
+                border: none;
+                background-color: #eff2f5;
+                width: 350px;
+                padding: .7rem;
+                margin-left: 1rem;
+            }
+
+            .ms_input i {
+                width: 60px;
+                height: 48px;
+                background-color: #447991;
+                line-height: 50px;
+                text-align: center;
+                color: lightgray;
+            }
+        }
+
+        .ms_login {
+            display: flex;
+            align-items: center;
+
+            i {
+                margin: 0 .4rem;
+                color: #447991;
+            }
+
+            span {
+                font-size: .9rem;
+            }
+
+            .user-login {
+                margin-left: 2rem;
+
+                button {
+                    padding: 1rem;
+                    border-radius: 30px;
+                    color: white;
+                    background-color: $brand-primary-color;
+                    border: none;
+                    margin: 0 .5rem;
+                }
+            }
+        }
     }
 
     hr {
