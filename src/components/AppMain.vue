006d9e66b65596e05da4d234118ade6c1d892e5f
vittorioaquilino <vittorioaquilino17@gmail.com>
2022-05-30 15:32:58 +0200

    add AppPreBanner

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,12 +1,19 @@
 <template>
-  <section></section>
+    <section class="ms_main">
+      <!-- pre banner -->
+      <AppPreBanner />
+      <!-- /pre banner -->
+
+    </section>
 </template>
 
 <script>
+import AppPreBanner from "./AppPreBanner.vue";
+
 export default {
     name: "AppMain",
     components: {
-        
+        AppPreBanner,
     }
 }
 </script>
---
+++ src/components/AppPreBanner.vue
@@ -0,0 +1,72 @@
+<template>
+  <section class="ms_container">
+      <ul class="icon-list d-flex">
+            <li
+            v-for="(element, index) in icons"
+            :key="index"
+            >
+                <i :class="element.icon"></i>
+                <span>{{ element.text }}</span>
+            </li>
+          
+      </ul>
+  </section>
+</template>
+
+<script>
+export default {
+    name: "AppPreBanner",
+    data() {
+        return {
+            icons: [
+                {
+                    icon: "far fa-chart-bar",
+                    text: "Business"
+                },
+                {
+                    icon: "fas fa-palette",
+                    text: "Design"
+                },
+                {
+                    icon: "fas fa-cog",
+                    text: "Development"
+                },
+                {
+                    icon: "far fa-smile",
+                    text: "Lifestyle"
+                },
+                {
+                    icon: "fas fa-wallet",
+                    text: "Office Productivity"
+                },
+            ]
+        }
+    }
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+.ms_container {
+    width: 100%;
+    background-color: #273043;
+}
+
+.icon-list {
+    width: 70%;
+    height: 100px;
+    align-items: center;
+    justify-content: center;
+    color: white;
+    margin: auto;
+
+    li {
+        width: calc(100% / 5);
+    }
+
+    i {
+        margin: 0 .4rem;
+    }
+}
+</style>
